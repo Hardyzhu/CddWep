42ce237f7d4872dcc74530dbf4018dd2af23c97f
maxu <75998373@qq.com>
2016-08-10 17:05:38 +0800

    星级管理页面

---
+++ CddWep/views/main/myStar/myStar.js
@@ -0,0 +1,13 @@
+/**
+ *  作者：maxu
+ *	时间：2016-08-09
+ *	描述：我的星级
+ */
+define(function(require){
+    var app = require('../../../app');
+
+    app.controller('myStarCrl',['$scope',function($scope){
+        $scope.title = '我的星级';
+    }]);
+});
+
---
+++ CddWep/views/main/starManage/starAssertNormal.js
@@ -0,0 +1,12 @@
+/**
+ *  作者：maxu
+ *	时间：2016-08-09
+ *	描述：星级评定标准
+ */
+define(function(require){
+    var app = require('../../../app');
+
+    app.controller('starAssertNormalCrl',['$scope',function($scope){
+        $scope.title = '星级评定标准';
+    }]);
+});
