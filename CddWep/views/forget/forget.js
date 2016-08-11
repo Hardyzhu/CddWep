14dbea665d92965068542c7946780644cdf2d1ef
陈浪 <978495506@qq.com>
2016-08-11 17:09:17 +0800

    忘记密码  登录

--- CddWep/views/forget/forget.js
+++ CddWep/views/forget/forget.js
@@ -1,16 +1,68 @@
 /**
  *  作者：yeshengqiang
- *	时间：2016-08-08
- *	描述：忘记密码
+ *    时间：2016-08-08
+ *    描述：忘记密码
  */
-define(function(require){
+define(function (require) {
     var app = require('../../app');
 
-    app.controller('forgetCrl',['$scope', '$location', '$state',function($scope,$location,$state){
+    app.controller('forgetCrl', ['$scope', '$location', '$state', function ($scope, $location, $state) {
 
-        $scope.register = function(){
-            //注册成功之后登陆
+        //登录
+        $scope.forget = function () {
+            var bool = true;
+            if ($scope.username == '' || $scope.username == null) {
+                layer.tips('请输入帐号', '#username', {
+                    tips: [1, '#3595CC'],
+                    time: 4000,
+                    tipsMore: true
+                });
+                bool = false;
+            }
+            if ($scope.email == '' || $scope.email == null) {
+                layer.tips('请输入邮箱', '#email', {
+                    tips: [1, '#3595CC'],
+                    time: 4000,
+                    tipsMore: true
+                });
+                bool = false;
+            } else {
+                if (!(/^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/).test($scope.email)) {
+                    layer.tips('邮箱格式不正确！', '#email', {
+                        tips: [1, '#3595CC'],
+                        time: 4000,
+                        tipsMore: true
+                    });
+                    bool = false;
+                }
+            }
+            if (!bool) {
+                return
+            }
+            //$.post('',JSON.stringify{username:$scope.username,email:$scope.email}).success(function(data){
+            if (0) {
+                layer.tips('帐号不存在', '#username', {
+                    tips: [1, '#3595CC'],
+                    time: 4000,
+                    tipsMore: true
+                });
+                return;
+            } else if (0) {
+                layer.tips('邮箱和帐号不匹配', '#password', {
+                    tips: [1, '#3595CC'],
+                    time: 4000,
+                    tipsMore: true
+                });
+                return;
+            } else if (0) {
+                layer.alert('找回密码失败，请稍候重试！', {icon: 2});
+                return;
+            }
+
             $location.path('/login');
+            //}).error(function(){
+            //	layer.alert('找回密码失败，请稍候重试！',{icon:2})
+            //});
         };
     }]);
 });
--- CddWep/views/login/login.js
+++ CddWep/views/login/login.js
@@ -7,9 +7,69 @@ define(function(require){
 	var app = require('../../app');
 
 	app.controller('loginCrl',['$scope', '$location', '$state','url',function($scope,$location,$state,url){
-        console.log(url);
+		//获取登录信息
+		$scope.loginInfo=sessionStorage.getItem('loginInfo');
+		if($scope.loginInfo!=null){
+			$scope.loginInfo = JSON.parse($scope.loginInfo);
+			$scope.username = $scope.loginInfo.username;
+			$scope.password = $scope.loginInfo.password;
+			$scope.rememberPassword = true;
+		}
+		//登录
 		$scope.login = function(){
-			$location.path('/main/baseInfo');
+			var bool = true ;
+			if($scope.username==''||$scope.username==null){
+				layer.tips('请输入用户名', '#username', {
+					tips: [1, '#3595CC'],
+					time: 4000,
+					tipsMore: true
+				});
+				bool = false;
+			}
+			if($scope.password==''||$scope.password==null){
+				layer.tips('请输入密码', '#password', {
+					tips: [1, '#3595CC'],
+					time: 4000,
+					tipsMore: true
+				});
+				bool = false;
+			}
+			if(!bool){
+				return
+			}
+			var loginInfo = {
+				username:$scope.username,
+				password:$scope.password
+			};
+			//$.post('',loginInfo).success(function(data){
+				if(0){
+					layer.tips('帐号不存在', '#username', {
+						tips: [1, '#3595CC'],
+						time: 4000,
+						tipsMore: true
+					});
+					return;
+				}else if(0){
+					layer.tips('密码错误', '#password', {
+						tips: [1, '#3595CC'],
+						time: 4000,
+						tipsMore: true
+					});
+					return;
+				}else if(0){
+					layer.alert('登录失败，请稍候重试！',{icon:2});
+					return;
+				}
+				//记住密码功能
+				if($scope.rememberPassword){
+					sessionStorage.setItem('loginInfo',JSON.stringify(loginInfo));
+				}else{
+					sessionStorage.removeItem('loginInfo');
+				}
+				$location.path('/main/baseInfo');
+			//}).error(function(){
+			//	layer.alert('登录失败，请稍候重试！',{icon:2})
+			//});
 		};
 	}]);
 });
