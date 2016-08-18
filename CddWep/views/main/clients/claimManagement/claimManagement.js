8cb1930cf5fdc0546533960d113975ef48746a79
yeshengqiang <673089899@qq.com>
2016-08-18 18:27:00 +0800

    修改路由及权限

--- CddWep/views/main/clients/claimManagement/claimManagement.js
+++ CddWep/views/main/clients/claimManagement/claimManagement.js
@@ -6,6 +6,25 @@
 define(function(require){
 	var app = require('../../../../app');
 	app.controller('claimManagementCrl',['$scope','$http','url',function($scope,$http,url){
+
+        //获取用户信息
+        var userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
+        //获取对应角色
+        var role = userInfo.data.type;                  //(1:品牌，2：物流，3：后台)
+        $scope.services = false;                        //服务项目(物流)
+        $scope.demand = false;                          //仓配需求(品牌)
+        $scope.parentTitle = '';                        //父标题
+        $scope.title = '';                              //子标题
+        if(role==1){
+            $scope.parentTitle = '我的服务商';
+            $scope.title = '保险理赔';
+            $scope.demand = true;
+        }else if(role==2){
+            $scope.parentTitle = '我的客户';
+            $scope.title = '理赔管理';
+            $scope.services = true;
+        }
+
 		//下拉菜单
 		$scope.selected ='';
 		$scope.dropdownItems=[
