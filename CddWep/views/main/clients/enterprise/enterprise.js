c11a999249f3572d7cd71f4abdcb1484b12b6115
yeshengqiang <673089899@qq.com>
2016-08-18 16:59:53 +0800

    新增html

---
+++ CddWep/views/main/clients/enterprise/enterprise.js
@@ -0,0 +1,12 @@
+/**
+ *  作者：yeshengqiang
+ *	时间：2016-08-09
+ *	描述：企业资质
+ */
+define(function(require) {
+    var app = require('../../../../app');
+
+    app.controller('enterpriseCrl',['$scope',function($scope){
+        $scope.title = '企业资质';
+    }]);
+});
