29e3a37183033fca80986223a335607be68921f8
yinxia <xia.yin@winphone.us>
2016-08-18 16:41:59 +0800

    咨询

--- CddWep/views/main/inquiryPlatform/inquiryPlatform.js
+++ CddWep/views/main/inquiryPlatform/inquiryPlatform.js
@@ -7,6 +7,7 @@ define(function(require){
     var app = require('../../../app');
 
     app.controller('inquiryPlatformCrl',['$scope',function($scope){
+        $scope.division = {"北京市":["东城区", "延庆县"], "上海市": ["黄浦区", "南汇区", "奉贤区", "崇明县"], "天津市": ["和平区", "静海县", "蓟县"]};
         $scope.title = '询价平台';
         yMake.fn.autoHeight('.bgWhite',45);
     }]);
