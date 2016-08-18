52d2e2669e4f22c8b0b9930ade6045adeba9d87f
yeshengqiang <673089899@qq.com>
2016-08-18 17:32:16 +0800

    修改权限

--- CddWep/views/main/clients/reports/returnData/returnData.js
+++ CddWep/views/main/clients/reports/returnData/returnData.js
@@ -7,6 +7,22 @@ define(function(require){
     var app = require('../../../../../app');
 
     app.controller('returnDataCrl',['$scope',function($scope){
+
+        //获取用户信息
+        var userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
+        //获取对应角色
+        var role = userInfo.data.type;                  //(1:品牌，2：物流，3：后台)
+        $scope.services = false;                        //服务项目(物流)
+        $scope.demand = false;                          //仓配需求(品牌)
+        $scope.parentTitle = '';                        //父标题
+        if(role==1){
+            $scope.parentTitle = '我的服务商';
+            $scope.demand = true;
+        }else if(role==2){
+            $scope.parentTitle = '我的客户';
+            $scope.services = true;
+        }
+
         $scope.title = '退货数据';
         yMake.fn.autoHeight('.bgWhite',45)
     }]);
