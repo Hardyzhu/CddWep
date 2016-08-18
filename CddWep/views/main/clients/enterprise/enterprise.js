a936cddf00f652f39e4ddbacbb78996c051c1fc2
yinxia <xia.yin@winphone.us>
2016-08-18 17:27:51 +0800

    企业资质

--- CddWep/views/main/clients/enterprise/enterprise.js
+++ CddWep/views/main/clients/enterprise/enterprise.js
@@ -7,6 +7,8 @@ define(function(require) {
     var app = require('../../../../app');
 
     app.controller('enterpriseCrl',['$scope',function($scope){
+        $scope.division = {"北京市":["东城区", "延庆县"], "上海市": ["黄浦区", "南汇区", "奉贤区", "崇明县"], "天津市": ["和平区", "静海县", "蓟县"]};
         $scope.title = '企业资质';
+        yMake.fn.autoHeight('.bgWhite',45);
     }]);
 });
