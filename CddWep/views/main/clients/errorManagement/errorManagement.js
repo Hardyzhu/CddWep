5b4d77eac89c71c531a3029fe524e6a4262fabdf
yeshengqiang <673089899@qq.com>
2016-08-18 17:42:41 +0800

    修改权限

--- CddWep/views/main/clients/errorManagement/errorManagement.js
+++ CddWep/views/main/clients/errorManagement/errorManagement.js
@@ -7,7 +7,23 @@ define(function (require) {
     var app = require('../../../../app');
 
     app.controller('errorManagementCrl', ['$scope', '$http', 'url', function ($scope, $http, url) {
-        $scope.title = '差错记录';
+
+
+        //获取用户信息
+        var userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
+        //获取对应角色
+        var role = userInfo.data.type;               //(1:品牌，2：物流，3：后台)
+        $scope.services = false;                        //服务项目(物流)
+        $scope.demand = false;                          //仓配需求(品牌)
+        $scope.parentTitle = '';                        //父标题
+        if(role==1){
+            $scope.parentTitle = '我的服务商';
+            $scope.demand = true;
+        }else if(role==2){
+            $scope.parentTitle = '我的客户';
+            $scope.services = true;
+        }
+
         $scope.division1 = [
             "类型1",
             "类型2",
