e14489d56b02b53f315fd9e42f08f3f106b02b02
yeshengqiang <673089899@qq.com>
2016-08-18 21:53:05 +0800

    修改部分权限及页面

--- CddWep/views/main/clients/enterprise/enterprise.js
+++ CddWep/views/main/clients/enterprise/enterprise.js
@@ -7,6 +7,22 @@ define(function(require) {
     var app = require('../../../../app');
 
     app.controller('enterpriseCrl',['$scope',function($scope){
+
+        //获取用户信息
+        var userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
+        //获取对应角色
+        var role = userInfo.data.type;               //(1:品牌，2：物流，3：后台)
+        $scope.services = false;                        //服务项目(物流)
+        $scope.demand = false;                          //仓配需求(品牌)
+        $scope.parentTitle = '';                        //父标题
+        if(role==1){
+            $scope.parentTitle = '我的服务商';
+            $scope.demand = true;
+        }else if(role==2){
+            $scope.parentTitle = '我的客户';
+            $scope.services = true;
+        }
+
         $scope.division = {"北京市":["东城区", "延庆县"], "上海市": ["黄浦区", "南汇区", "奉贤区", "崇明县"], "天津市": ["和平区", "静海县", "蓟县"]};
         $scope.title = '企业资质';
         yMake.fn.autoHeight('.bgWhite',45);
