c5d9dd60e01204b56a78a310eaa602a4a92ccdaa
zhangqian <1139384196@qq.com>
2016-08-18 15:36:28 +0800

    问题反馈，合同条款

--- CddWep/views/main/clients/clause/clause.js
+++ CddWep/views/main/clients/clause/clause.js
@@ -5,11 +5,21 @@
  */
 define(function(require){
 	var app = require('../../../../app');
-
 	app.controller('serviceTeamCrl',['$scope','url','$http',function($scope,url,$http){
 		$scope.division = {"北京市":["东城区", "延庆县"], "上海市": ["黄浦区", "南汇区", "奉贤区", "崇明县"], "天津市": ["和平区", "静海县", "蓟县"]};
-		$scope.title = '合同条款';
 
+		$scope.items= [
+			{
+				pactno:'ZA27792893',
+				brandedcompanyid:'嘀嘀嘀哒',
+				pactname:'租赁合同',
+				pactoperant:'2016/8/30',
+				deadline:'2017/8/30',
+				pactscan:'http://www.baidu.com',
+				extrapact:'税务合同'
+			}
+
+		];
 		function load(){
 			var currentCheck = function(page,callback){
 				$http.post(url+'/warehouse/user/hyquery2Page',
--- CddWep/views/main/problemAnswer/problemAnswer.js
+++ CddWep/views/main/problemAnswer/problemAnswer.js
@@ -7,7 +7,19 @@ define(function(require){
     var app = require('../../../app');
 
     app.controller('problemAnswerCrl',['$scope','url','$http',function($scope,url,$http){
-        $scope.title = '问题反馈';
+        //$scope.title = '问题反馈';
+
+        $scope.items= [
+            {
+                id:'1',
+                type:'物流',
+                detail:'如何查询物流信息',
+                fkdate:'2016/8/38',
+                response:'已解决'
+            }
+
+        ];
+
         var answerList = function(page,callback){
             $http.post(url+'/warehouse/suggestion/showPageList',{pageSize:page}).success(callback);
         };
