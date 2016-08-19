9751cb8c6aa41c476a912442c6cf3a8442eb8128
陈浪 <978495506@qq.com>
2016-08-19 16:20:54 +0800

    新增城配，仓储，干线服务接口

--- CddWep/views/main/baseInfo/baseInfoNew/baseInfoNew.js
+++ CddWep/views/main/baseInfo/baseInfoNew/baseInfoNew.js
@@ -5,10 +5,11 @@
  */
 define(function(require){
     var app = require('../../../../app');
-    console.log(app);
-    app.controller('baseInfoNewCrl',['$scope','url',function($scope,url){
+    app.controller('baseInfoNewCrl',['$scope','url','$http',function($scope,url,$http){
         //添加图片
-        $scope.uploadPhoto = function(){
+        var urls = [];//资质文件路径
+        $scope.uploadPhoto = function(index){
+            var img = $('#'+index);
             $('#example').modal({backdrop:'static'});
             $('#upload').empty().append('<div id="zyUpload"></div>');
             $("#zyUpload").zyUpload({
@@ -16,7 +17,7 @@ define(function(require){
                 height           :   "100%",                 // 宽度
                 itemWidth        :   "140px",                 // 文件项的宽度
                 itemHeight       :   "115px",                 // 文件项的高度
-                url              :   url+"/upload/UploadAction",  // 上传文件的路径
+                url              :   url+"/file/upload",  // 上传文件的路径
                 fileType         :   ["jpg","png","txt","js","exe"],// 上传文件的类型
                 fileSize         :   51200000,                // 上传文件的大小
                 multiple         :   true,                    // 是否可以多个文件上传
@@ -33,12 +34,25 @@ define(function(require){
                     console.info("当前删除了此文件：");
                     console.info(file.name);
                 },
-                onSuccess: function(file, response){          // 文件上传成功的回调方法
-                    console.info("此文件上传成功：");
-                    console.info(file.name);
-                    console.info("此文件上传到服务器地址：");
-                    console.info(response);
-                    $("#uploadInf").append("<p>上传成功，文件地址是：" + response + "</p>");
+                onSuccess: function(file, response) {          // 文件上传成功的回调方法
+                    // 文件上传成功的回调方法
+                    var fileName = JSON.parse(response).data, photoUrl = url + '/' + fileName, src = img.children().attr('src');
+                    if(index=='img'){
+                        $scope.storage.storageform = fileName;
+                    }else if(index=='carInfoFile1'){
+                        $scope.cityDelivery.trunkimg = fileName;
+                    }else if(index=='carInfoFile0'){
+                        $scope.trunkLine.trunkimg = fileName;
+                    }else{
+                        if (urls.length > 0 && urls.indexOf(fileName) != -1) {
+
+                        } else if (src != null) {
+                            urls.splice(urls.indexOf(src.substring(src.lastIndexOf('upload'))), 1, fileName)
+                        } else {
+                            urls.push(fileName)
+                        }
+                        img.empty().append("<img src=" + photoUrl + " width='100%' height='100%'/>");
+                    }
                 },
                 onFailure: function(file, response){          // 文件上传失败的回调方法
                     console.info("此文件上传失败：");
@@ -50,7 +64,62 @@ define(function(require){
                 }
             });
         };
+
+        //获取所有的省
+        $http.get(url+'/location/loadProvince').success(function(data){
+            $scope.provinces = data.data;
+        });
+        //根据省id获取城市
+        $scope.getCity = function(province){
+            $http.get(url+'/location/loadCity?id='+province).success(function(data){
+                $scope.cities = data.data;
+            })
+        };
+
+        //新增仓储服务
+        $scope.storage = {};
+        $scope.addStorage = function(){
+            $scope.storage.storageimg = urls.join(',');
+            $http.post(url+'/storage/add',$scope.storage).success(function(data){
+
+            }).error(function(){
+                yMake.layer.msg('新增仓储服务出错！')
+            })
+        };
+        //新增城配服务
+        $scope.cityDelivery = {};
+        $scope.addCityDelivery = function(){
+            $scope.cityDelivery.type=1;
+            $scope.cityDelivery.delivery = $scope.cityDelivery.deliveryStart + '~'+$scope.cityDelivery.deliveryEnd;
+            $http.post(url+'/dryline/add',$scope.cityDelivery).success(function(data){
+                if(data.code==0){
+                    yMake.layer.msg('新增城配服务成功！');
+                    $scope.cityDelivery = {};
+                }else{
+                    yMake.layer.msg('新增城配服务失败！')
+                }
+            }).error(function(){
+                yMake.layer.msg('新增城配服务出错！')
+            })
+        };
+        //新增干线服务
+        $scope.trunkLine = {};
+        $scope.addTrunkLine = function(){
+            $scope.trunkLine.type=0;
+            $scope.trunkLine.delivery = $scope.trunkLine.deliveryStart + '~'+$scope.trunkLine.deliveryEnd;
+            $http.post(url+'/dryline/add',$scope.trunkLine).success(function(data){
+                if(data.code==0){
+                    yMake.layer.msg('新增干线服务成功！');
+                    $scope.trunkLine = {};
+                }else{
+                    yMake.layer.msg('新增干线服务失败！')
+                }
+
+            }).error(function(){
+                yMake.layer.msg('新增干线服务出错！')
+            })
+        };
         //获取浏览器的高度
-        yMake.fn.autoHeight('.bgWhite',45);
+        //yMake.fn.autoHeight('.bgWhite',45);
     }]);
 });
