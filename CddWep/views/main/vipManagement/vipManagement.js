91087a7089fe6a2e461f4cbd0b4e2931972246dc
yeshengqiang <673089899@qq.com>
2016-08-19 15:11:51 +0800

    会员管理

--- CddWep/views/main/vipManagement/vipManagement.js
+++ CddWep/views/main/vipManagement/vipManagement.js
@@ -6,7 +6,47 @@
 define(function(require){
     var app = require('../../../app');
 
-    app.controller('vipManagementCrl',['$scope',function($scope){
-        $scope.title='会员管理';
+    app.filter('stateFormat',function(){
+        //0 未认证,2 认证不通过,1 认证,3 拉黑
+        return function(inp){
+            var info = '';
+            switch (inp){
+                case '1':
+                    info = '已认证';
+                    break;
+                case '2':
+                    info = '认证不通过';
+                    break;
+                case '3':
+                    info = '已拉黑';
+                    break;
+                default :
+                    info = '未认证';
+                    break;
+            }
+            return info;
+        }
+    });
+
+    app.controller('vipManagementCrl',['$scope','url','$http',function($scope,url,$http){
+
+        //条件
+        $scope.division = [
+            {value:0,name:'未认证'},
+            {value:1,name:'已认证'},
+            {value:2,name:'认证不通过'},
+            {value:3,name:'已拉黑'},
+        ];
+
+        //分页查询
+        var fetchFunction = function(page,callback){
+            $http.post(url+'/user/hyquery2Page', $.extend({},page,$scope.searchData)).success(callback)
+        };
+        $scope.searchPaginator = app.get('Paginator').list(fetchFunction,6);
+
+        //审核
+        $scope.audit = function(id){
+            console.log(id);
+        };
     }]);
 });
