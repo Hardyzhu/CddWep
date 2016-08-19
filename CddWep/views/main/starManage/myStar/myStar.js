453b92af288e77efe61511558bc66890bd5971c0
mgfshut <mgfshut@vip.qq.com>
2016-08-19 21:39:49 +0800

    1

--- CddWep/views/main/starManage/myStar/myStar.js
+++ CddWep/views/main/starManage/myStar/myStar.js
@@ -10,7 +10,17 @@ define(function(require){
         $scope.title = '我的星级';
         yMake.fn.autoHeight('.bgWhite',45);
         
-        $scope.searchPaginator = [
+        /*//获取分页数据
+        function load(){
+           var fetchFunction = function(page,callback){
+              $http.post(url+'/pact/showPageList', $.extend({},page,{})).success(callback)
+           };
+           $scope.searchPaginator = app.get('Paginator').list(fetchFunction,6);
+        }
+        load();*/
+
+        
+        $scope.arr = [
           			{a:'30',b:'25',c:'20',d:'10',e:'83',f:'三级',g:'456'},
           			{a:'30',b:'25',c:'20',d:'10',e:'67',f:'二级',g:'322'},
           			{a:'30',b:'25',c:'20',d:'10',e:'56',f:'二级',g:'234'},
--- CddWep/views/main/starManage/starAssertNormal.js
+++ CddWep/views/main/starManage/starAssertNormal.js
@@ -15,6 +15,15 @@ define(function(require){
         		err21:'10%',err22:'60%',
         		err31:'0',err32:'10%',
         		fen1:'20',fen2:'50',fen3:'100'};
+        
+        /*function load(){
+        	 $http.post(url+'/FinancialBankNum/updateFinancialBankNum',{FinancialBankNum:$scope.financialBankNum}).success(function(data){
+        		 $scope.financialBankNum=data.FinancialBankNum;
+             }).error(function(data, status, headers, config){
+                 layer.msg("查询失败",{icon:2});
+             });
+         }
+         load();*/
 
     }]);
 });
