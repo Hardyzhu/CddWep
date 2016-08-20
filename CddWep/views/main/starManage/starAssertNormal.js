69694c6b506c5d1c8feada09446095e4cfefa011
mgfshut <mgfshut@vip.qq.com>
2016-08-20 17:11:27 +0800

    1

--- CddWep/views/main/starManage/starAssertNormal.js
+++ CddWep/views/main/starManage/starAssertNormal.js
@@ -6,7 +6,7 @@
 define(function(require){
     var app = require('../../../app');
 
-    app.controller('starAssertNormalCrl',['$scope',function($scope){
+    app.controller('starAssertNormalCrl',['$scope','url','$http','$location',function($scope,url,$http,$location){
         $scope.title = '星级评定标准';
         yMake.fn.autoHeight('.bgWhite',45);
 
@@ -16,14 +16,16 @@ define(function(require){
         		err31:'0',err32:'10%',
         		fen1:'20',fen2:'50',fen3:'100'};
         
-        /*function load(){
-        	 $http.post(url+'/FinancialBankNum/updateFinancialBankNum',{FinancialBankNum:$scope.financialBankNum}).success(function(data){
-        		 $scope.financialBankNum=data.FinancialBankNum;
+        function load(){
+        	$scope.demo = [];
+        	 $http.post(url+'/score/selectAll',{Score:$scope.demo}).success(function(data){
+        		 $scope.Score=data.Score;
+        		 console.log($scope.Score);
              }).error(function(data, status, headers, config){
                  layer.msg("查询失败",{icon:2});
              });
          }
-         load();*/
+         load();
 
     }]);
 });
