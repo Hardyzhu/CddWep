27281d43ef74bd7c34fd85cc2fa98afaed8b732f
Emma <1245095397@qq.com>
2016-08-23 15:58:11 +0800

    理赔时效管理

--- CddWep/views/main/clients/reports/agingData/agingData.js
+++ CddWep/views/main/clients/reports/agingData/agingData.js
@@ -12,20 +12,34 @@ define(function(require){
         var userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
         //获取对应角色
         var role = userInfo.data.type;                  //(1:品牌，2：物流，3：后台)
-        $scope.services = false;                        //服务项目(物流)
         $scope.demand = false;                          //仓配需求(品牌)
-        $scope.parentTitle = '';                        //父标题
+        $scope.services = false;                        //服务项目(物流)
+        $scope.backManage = false;                      //后台管理(后台)
+        $scope.parentTitle = '';                        //一级父标题
+        $scope.fatherTitle = '';                        //二级父标题
+        $scope.title = '';                              //子标题
+        //1 品牌   我的服务商   运营报表   时效数据
+        //2 物流   我的客户  运营报表  时效数据
+        //3 后台   品质中心  时效管理
         if(role==1){
-            $scope.parentTitle = '我的服务商';
+            $scope.parentTitle ='我的服务商';
+            $scope.fatherTitle ='运营报表';
+            $scope.title ='时效数据';
             $scope.demand = true;
         }else if(role==2){
             $scope.parentTitle = '我的客户';
+            $scope.fatherTitle ='运营报表';
+            $scope.title ='时效数据';
             $scope.services = true;
+        }else if(role==3){
+            $scope.parentTitle ='品质中心';
+            $scope.title ='时效管理';
+            $scope.backManage = true;
         }
 
         //获取分页数据
         var currentCheck = function (page, callback) {
-            $http.post(url + '/efficiency/showPageList', $.extend({}, page, {})).success(callback);
+            $http.post(url + '/efficiency/showPageList', $.extend({}, page, $scope.searchData)).success(callback);
         };
         $scope.projectItem = app.get('Paginator').list(currentCheck, 6);
         console.log($scope.projectItem);
@@ -37,7 +51,5 @@ define(function(require){
             window.location.href=url+'/efficiency/export';
         };
 
-        $scope.title = '时效管理';
-        yMake.fn.autoHeight('.bgWhite',45)
     }]);
 });
