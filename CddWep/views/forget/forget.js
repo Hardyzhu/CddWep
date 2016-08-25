603e69e5c0aa97ebf2d7974c2b0d6a447f42f980
zhangqian <1139384196@qq.com>
2016-08-25 18:41:23 +0800

    忘记密码

--- CddWep/views/forget/forget.js
+++ CddWep/views/forget/forget.js
@@ -11,56 +11,37 @@ define(function (require) {
         //登录
         $scope.forget = function () {
             if ($scope.username == '' || $scope.username == null) {
-                layer.tips('请输入帐号', '#username', {
-                    tips: [1, '#3595CC'],
-                    time: 4000,
-                    tipsMore: true
-                });
-                return
+                yMake.layer.msg('请输入账号',{icon:0});
+                return;
             }
             if ($scope.email == '' || $scope.email == null) {
-                layer.tips('请输入邮箱', '#email', {
-                    tips: [1, '#3595CC'],
-                    time: 4000,
-                    tipsMore: true
-                });
-                return
+                yMake.layer.msg('请输入邮箱',{icon:0});
+                return;
             } else {
                 var reg = /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/;
                 if (!reg.test($scope.email)) {
-                    layer.tips('邮箱格式不正确！', '#email', {
-                        tips: [1, '#3595CC'],
-                        time: 4000,
-                        tipsMore: true
-                    });
-                    return
+                    yMake.layer.msg('邮箱格式不正确',{icon:0});
+                    return;
                 }
             }
-            $.post('',JSON.stringify,{username:$scope.username,email:$scope.email}).success(function(data) {
+            $.post(url + '/user/resetpwd',{loginname:$scope.username,email:$scope.email}).success(function(data) {
                 console.log(data);
-
-            if (0) {
-                layer.tips('帐号不存在', '#username', {
-                    tips: [1, '#3595CC'],
-                    time: 4000,
-                    tipsMore: true
-                });
+            if (data.loginname) {
+                yMake.layer.msg('账号不存在',{icon:0});
+                return;
+            }
+                if (0) {
+                yMake.layer.msg('邮箱与账号不匹配',{icon:0});
                 return;
             } else if (0) {
-                layer.tips('邮箱和帐号不匹配', '#password', {
-                    tips: [1, '#3595CC'],
-                    time: 4000,
-                    tipsMore: true
-                });
-                return;
-            } else if (0) {
-                layer.alert('找回密码失败，请稍候重试！', {icon: 2});
+                yMake.layer.msg('找回密码失败，请重试',{icon:0});
                 return;
             }
 
             $location.path('/login');
             }).error(function(){
-            	layer.alert('找回密码失败，请稍候重试！',{icon:2})
+                yMake.layer.msg('找回密码失败，请重试',{icon:0});
+                return;
             });
         };
     }]);
