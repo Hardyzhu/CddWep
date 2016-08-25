6f38a727d39e3f7aff63097f0f9279724f6878b4
Emma <1245095397@qq.com>
2016-08-25 11:13:04 +0800

    投诉管理页面完善！

--- CddWep/views/main/clients/complaintRecord/complaintRecord.js
+++ CddWep/views/main/clients/complaintRecord/complaintRecord.js
@@ -10,10 +10,6 @@ define(function(require){
         //获取用户信息
         var userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
         //获取对应角色
-        var role = userInfo.data.type;               //(1:品牌，2：物流，3：后台)
-        $scope.services = false;                        //服务项目(物流)
-        $scope.demand = false;                          //仓配需求(品牌)
-        $scope.parentTitle = '';                        //父标题
         var role = userInfo.data.type;                  //(1:品牌，2：物流，3：后台)
         $scope.services = false;                        //服务项目(物流)
         $scope.demand = false;                          //仓配需求(品牌)
