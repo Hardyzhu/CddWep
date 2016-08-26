71e6bd35fa2b9cb2788f78585c0d69df7fc9d28a
Emma <1245095397@qq.com>
2016-08-26 14:43:12 +0800

    早安1919-物流下载！

--- CddWep/views/main/goodMorning/goodMorning.js
+++ CddWep/views/main/goodMorning/goodMorning.js
@@ -11,18 +11,18 @@ define(function(require){
         //获取用户信息
         var userInfo = JSON.parse(sessionStorage.getItem('userInfo')),checkVlaue = app.get('checkValue');
         //获取对应角色
-        var role = userInfo.data.type;                  //(1:品牌，2：物流，3：后台)
+        var role = userInfo.data.type;               //(1:品牌，2：物流，3：后台)
         $scope.services = false;                        //服务项目(物流)
         $scope.demand = false;                          //仓配需求(品牌)
-        $scope.parentTitle = '';                        //父标题
         if(role==1){
-            $scope.parentTitle = '我的服务商';
             $scope.demand = true;
-            loadPP();
+            demFun();
         }else if(role==2){
-            $scope.parentTitle = '我的客户';
             $scope.services = true;
-            loadWL();
+            serFun();
+        }else if(role==3){
+            $scope.backstage = true;
+            bacFun();
         }
         //物流分页
         $scope.searchData = {};
