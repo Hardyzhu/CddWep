d631aa2c9c6381562a1b7f8b1bcb4b6b1a6f81a8
zhangqian <1139384196@qq.com>
2016-08-29 13:42:51 +0800

    21

--- CddWep/views/main/clients/reports/outPut/outPut.js
+++ CddWep/views/main/clients/reports/outPut/outPut.js
@@ -1,28 +1,74 @@
 /**
  *  作者：yeshengqiang
- *	时间：2016-08-09
- *	描述：出入库报表
+ *    时间：2016-08-09
+ *    描述：出入库报表
  */
-define(function(require){
+define(function (require) {
     var app = require('../../../../../app');
-
-    app.controller('outPutCrl',['$scope',function($scope){
+    //过滤器
+    app.filter('typeFormat', function () {
+        return function (inp) {
+            //类型暂未给出
+            var info = "";
+            switch (inp) {
+                case '1':
+                    info = '入库';
+                    break;
+                case '2':
+                    info = '出库';
+                    break;
+            }
+            return info;
+        };
+    });
+    app.controller('outPutCrl', ['$scope', 'url', '$http', function ($scope, url, $http) {
 
         //获取用户信息
         var userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
         //获取对应角色
         var role = userInfo.data.type;                  //(1:品牌，2：物流，3：后台)
-        $scope.services = false;                        //服务项目(物流)
-        $scope.demand = false;                          //仓配需求(品牌)
+        $scope.transport = false;                        //(物流)
+        $scope.brand = false;                          //(品牌)
         $scope.parentTitle = '';                        //父标题
-        if(role==1){
+        if (role == 1) {
             $scope.parentTitle = '我的服务商';
-            $scope.demand = true;
-        }else if(role==2){
+            $scope.brand = true;
+        } else if (role == 2) {
             $scope.parentTitle = '我的客户';
-            $scope.services = true;
+            $scope.transport = true;
         }
+        //获取所有的省
+        $http.get(url + '/location/loadProvince').success(function (data) {
+            $scope.provinces = data.data;
+        });
+        //根据省id获取城市
+        $scope.getCity = function (province) {
+            $scope.searchData.city = '';
+            $http.get(url + '/location/loadCity?id=' + province).success(function (data) {
+                $scope.cities = data.data;
+                console.log($scope.cities);
+            })
+        };
 
-        yMake.fn.autoHeight('.bgWhite',45)
+
+        //分页
+        $scope.searchData = {};
+        var currentCheck = function (page, callback) {
+            console.log(page);
+            console.log($scope.searchData);
+            var param = app.get('checkValue').searchData($scope.searchData);
+            console.log(param);
+            $http.post(url + '/outinput/showPageList?loginname='+'tf', $.extend({},page,param)).success(callback);
+        };
+        $scope.searchPaginator = app.get('Paginator').list(currentCheck, 6);
+        console.log($scope.searchPaginator);
+
+
+        //导出
+        $scope.downloadFile = function () {
+
+            window.location.href = url + '/outinput/export?type=' + '1';
+        };
+
     }]);
 });
