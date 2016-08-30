91abb12028f2a99cef9111023eb9cb7a19d9ef6a
zhangqian <1139384196@qq.com>
2016-08-30 16:05:47 +0800

    出入库报表查询第一版

--- CddWep/views/main/clients/reports/outPut/outPut.js
+++ CddWep/views/main/clients/reports/outPut/outPut.js
@@ -5,22 +5,6 @@
  */
 define(function (require) {
     var app = require('../../../../../app');
-    //过滤器
-    app.filter('typeFormat', function () {
-        return function (inp) {
-            //类型暂未给出
-            var info = "";
-            switch (inp) {
-                case '1':
-                    info = '入库';
-                    break;
-                case '2':
-                    info = '出库';
-                    break;
-            }
-            return info;
-        };
-    });
     app.controller('outPutCrl', ['$scope', 'url', '$http', function ($scope, url, $http) {
 
         //条件
@@ -45,7 +29,6 @@ define(function (require) {
             $scope.transport = true;
         }
 
-
         //日报表查询分页
         $scope.searchData = {};
         var currentCheck = function (page, callback) {
@@ -53,7 +36,7 @@ define(function (require) {
             console.log($scope.searchData);
             var param = app.get('checkValue').searchData($scope.searchData);
             console.log(param);
-            $http.post(url + '/outinput/showPageListD', $.extend({},page,param)).success(callback);
+            $http.post(url + '/outinput/showPageListD', $.extend({}, page, param)).success(callback);
         };
         $scope.searchDadilyPaginator = app.get('Paginator').list(currentCheck, 6);
         console.log($scope.searchDadilyPaginator);
@@ -63,24 +46,29 @@ define(function (require) {
             console.log($scope.searchData);
             var param = app.get('checkValue').searchData($scope.searchData);
             console.log(param);
-            $http.post(url + '/outinput/showPageListM', $.extend({},page,param)).success(callback);
+            $http.post(url + '/outinput/showPageListM', $.extend({}, page, param)).success(callback);
         };
         $scope.searchMonthlyPaginator = app.get('Paginator').list(currentCheck, 6);
         console.log($scope.searchMonthlyPaginator);
         //状态
-        var state=1;
-        $scope.changeState1= function (){
-            state=1;
+        var state = 1;
+        $scope.changeState1 = function () {
+            state = 1;
+            $scope.searchDadilyPaginator._load();
         };
-        $scope.changeState2= function (){
-            state=2;
+        $scope.changeState2 = function () {
+            state = 2;
+            $scope.searchMonthlyPaginator._load();
         };
         //搜索
 
         $scope.search = function () {
-                //日报表查询分页
-                $scope.searchData = {};
-                switch (+state){
+            //日报表查询分页
+            if($scope.searchData.types==''||$scope.searchData.types==null){
+                yMake.layer.msg('请补全搜索类型',{icon:2});
+                return;
+            }
+                switch (+state) {
                     case 1:
                         $scope.searchDadilyPaginator._load();
                         break;
@@ -92,8 +80,64 @@ define(function (require) {
 
         //导出
         $scope.downloadFile = function () {
-
+            if($scope.searchData.types==''||$scope.searchData.types==null){
+                yMake.layer.msg('请补全搜索类型',{icon:2});
+                return;
+            }
             window.location.href = url + '/outinput/export?type=' + '1';
+        };
+        ////查看日报表明细
+        //$scope.dailyCheck = function (item) {
+        //    $rootScope.output = item;
+        //    $location.path('/main/clients/reports/outPut');
+        //};
+        ////查看月报表明细
+        //$scope.dailyCheck = function (item) {
+        //    $rootScope.output = item;
+        //    $location.path('/main/clients/reports/outPut');
+        //};
+        //查看日报表明细
+        $scope.dailyCheck = function (item) {
+            $('#daily').modal({backdrop: 'static', keyboard: false});
+            $scope.modalTitle = '查看日报表明细';
+            console.log(item);
+            $scope.daily = {};
+            $scope.daily.storagecode = item.storagecode;
+            $scope.daily.storagename = item.storagename;
+            $scope.daily.goodscode = item.goodscode;
+            $scope.daily.goodsname = item.goodsname;
+            $scope.daily.amount = item.amount;
+            $scope.daily.weight = item.weight;
+            $scope.daily.volum = item.volum;
+            $scope.daily.shdate = item.shdate;
+            $scope.daily.fydate = item.fydate;
+            if (item.types == 1) {
+                $scope.daily.types = "入库";
+            } else {
+                $scope.daily.types = "出库";
+            }
+        };
+
+        //查看月报表明细
+        $scope.monthlyCheck = function (item) {
+            $('#monthly').modal({backdrop: 'static', keyboard: false});
+            $scope.modalTitle = '查看月报表明细';
+            console.log(item);
+            $scope.monthly = {};
+            $scope.monthly.storagecode = item.storagecode;
+            $scope.monthly.storagename = item.storagename;
+            $scope.monthly.goodscode = item.goodscode;
+            $scope.monthly.goodsname = item.goodsname;
+            $scope.monthly.amount = item.amount;
+            $scope.monthly.weight = item.weight;
+            $scope.monthly.volum = item.volum;
+            $scope.monthly.shdate = item.shdate;
+            $scope.monthly.fydate = item.fydate;
+            if (item.types == 1) {
+                $scope.monthly.types = "入库";
+            } else {
+                $scope.monthly.types = "出库";
+            }
         };
         yMake.fn.autoHeight('.bgWhite',45);
     }]);
