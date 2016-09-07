3d0c0c5e347b905cd30276c16f0e2589a858c01a
zhangqian <1139384196@qq.com>
2016-09-07 14:16:03 +0800

    客户账户，平台账单页面修改

--- CddWep/views/main/qualityCenter/complaintManagement/complaintAnalyze/complaintAnalyze.js
+++ CddWep/views/main/qualityCenter/complaintManagement/complaintAnalyze/complaintAnalyze.js
@@ -1,13 +1,13 @@
 /**
  *  作者：cl
- *	时间：2016-08-10
- *	描述：差错管理
+ *    时间：2016-08-10
+ *    描述：差错管理
  */
-define(function(require){
+define(function (require) {
     var app = require('../../../../../app');
 
-    app.controller('complaintAnalyzeCrl',['$scope','$http','url',function($scope,$http,url){
-        $scope.title='投诉数据分析';
+    app.controller('complaintAnalyzeCrl', ['$scope', '$http', 'url', function ($scope, $http, url) {
+        $scope.title = '投诉数据分析';
 
         //初始化
         $scope.searchData = {};
@@ -15,25 +15,26 @@ define(function(require){
         //获取分页数据
         var currentCheck = function (page, callback) {
             var param = app.get('checkValue').dateRangeFormat($scope.searchData);
-            $http.post(url + '/complaint/query2Tj', $.extend({},page, param)).success(callback);
+            console.log(param);
+            $http.post(url + '/complaint/query2Tj', $.extend({}, page, param)).success(callback);
         };
         $scope.complaintAnalyze = app.get('Paginator').list(currentCheck, 6);
-
+        console.log($scope.complaintAnalyze );
         //导出
-        $scope.downloadFile = function(){
+        $scope.downloadFile = function () {
             var teamInfo = {
                 //brandedcompanyid: $scope.brandedcompanyid,
                 //city: $scope.city,
                 //province: $scope.province
             };
             layer.confirm("是否导出文件？",
-                {btn : ['是','否']},function(){
-                    window.open(url+'/complaint/export2Tj?teamInfo='+JSON.stringify(teamInfo),'_top');
-                    yMake.layer.msg("导出总结文件成功 ",{icon:1,time:1000});
-                    layer.msg("",{time:1});
+                {btn: ['是', '否']}, function () {
+                    window.open(url + '/complaint/export2Tj?teamInfo=' + JSON.stringify(teamInfo), '_top');
+                    yMake.layer.msg("导出总结文件成功 ", {icon: 1, time: 1000});
+                    layer.msg("", {time: 1});
                 });
 
         };
-        yMake.fn.autoHeight('.bgWhite',45);
+        yMake.fn.autoHeight('.bgWhite', 45);
     }]);
 });
